91cf69641868c119143d930aef9df0bab287b1a4
litongjava <litongjava@qq.com>
2025-02-04 19:17:58 -1000

    update apiChatController

--- src/main/java/com/litongjava/perplexica/controller/ApiChatsController.java
+++ src/main/java/com/litongjava/perplexica/controller/ApiChatsController.java
@@ -1,7 +1,11 @@
 package com.litongjava.perplexica.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
 import com.jfinal.kit.Kv;
 import com.litongjava.annotation.Get;
 import com.litongjava.annotation.RequestPath;
@@ -9,6 +13,8 @@ import com.litongjava.jfinal.aop.Aop;
 import com.litongjava.perplexica.model.PerplexicaChatMessage;
 import com.litongjava.perplexica.services.ChatMessgeService;
 import com.litongjava.perplexica.services.ChatsService;
+import com.litongjava.perplexica.vo.WebPageSource;
+import com.litongjava.tio.utils.json.FastJson2Utils;
 
 @RequestPath("/api/chats")
 public class ApiChatsController {
@@ -25,7 +31,26 @@ public class ApiChatsController {
   public Kv get(Long id) {
     Kv kv = Kv.by("chat", chatsService.getById(id));
     List<PerplexicaChatMessage> listMessage = chatMessgeService.listMessage(id);
-    kv.set("messages", listMessage);
+    List<Map<String, Object>> newMessages = new ArrayList<>();
+    for (PerplexicaChatMessage perplexicaChatMessage : listMessage) {
+      Map<String, Object> map = perplexicaChatMessage.toMap();
+      String sourcesStr = perplexicaChatMessage.getSources();
+      if (sourcesStr != null) {
+        JSONArray jsonArray = FastJson2Utils.parseArray(sourcesStr);
+        List<WebPageSource> sources = new ArrayList<>();
+        for (int i = 0; i < jsonArray.size(); i++) {
+          JSONObject jsonObject = jsonArray.getJSONObject(i);
+          String title = jsonObject.getString("title");
+          String url = jsonObject.getString("url");
+          String content = jsonObject.getString("content");
+          WebPageSource webPageSource = new WebPageSource(title, url,content);
+          sources.add(webPageSource);
+        }
+        map.put("sources", sources);
+      }
+      newMessages.add(map);
+    }
+    kv.set("messages", newMessages);
     return kv;
   }
 }
