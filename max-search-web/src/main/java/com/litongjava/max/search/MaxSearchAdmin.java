abe5d0dc08f04a69beab2265f46947086e98a663
litongjava <litongjava@qq.com>
2025-03-24 00:07:58 -1000

    add --download to MaxSearchAdmin

--- max-search-web/src/main/java/com/litongjava/max/search/MaxSearchAdmin.java
+++ max-search-web/src/main/java/com/litongjava/max/search/MaxSearchAdmin.java
@@ -2,15 +2,33 @@ package com.litongjava.max.search;
 
 import com.litongjava.annotation.AComponentScan;
 import com.litongjava.tio.boot.TioApplication;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.Playwright;
 
 @AComponentScan
 public class MaxSearchAdmin {
   public static void main(String[] args) {
-    long start = System.currentTimeMillis();
-    //HotSwapResolver.addSystemClassPrefix("om.litongjava.perplexica.vo.");
-    TioApplication.run(MaxSearchAdmin.class, args);
-    //    TioApplication.run(PerplexicaAdmin.class, args);
-    long end = System.currentTimeMillis();
-    System.out.println((end - start) + "ms");
+    boolean download = false;
+    for (String string : args) {
+      if ("--download".equals(string)) {
+        download = true;
+        break;
+      }
+    }
+    if (download) {
+      System.out.println("download start");
+      LaunchOptions launchOptions = new BrowserType.LaunchOptions().setHeadless(true); // 使用无头模式
+      try (Playwright playwright = Playwright.create(); Browser launch = playwright.chromium().launch(launchOptions)) {
+      }
+      System.out.println("download end");
+    } else {
+      long start = System.currentTimeMillis();
+      //HotSwapResolver.addSystemClassPrefix("om.litongjava.perplexica.vo.");
+      TioApplication.run(MaxSearchAdmin.class, args);
+      long end = System.currentTimeMillis();
+      System.out.println((end - start) + "ms");
+    }
   }
 }
